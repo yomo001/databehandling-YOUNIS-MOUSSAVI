f25e58ab84a5c70d4d570f8b281a4faee973be5b
Younis Moussavi <yonismoussawi@hotmail.com>
2023-11-16 14:57:26 +0100

    More lecture notes

--- Code-Alongs/L018_Dash_intro.py
+++ Code-Alongs/L018_Dash_intro.py
@@ -8,59 +8,86 @@
 
 from dash import Dash, html, dcc, callback, Output, Input
 
+# Tre delar man egentligen vill göra
 
-app = Dash("__name__") # skapar en instans av Dash
+# hmtl komponenter, dash har egna också
+# layout
+# Funktionalitet som gör att ngt händer när man klickar någonstans: callbacks
+
+
+
+
+app = Dash("__name__") # skapar en instans av Dash klassen
 # __name__ är namnet på filen som körs.
 
 
 
-my_H1 = app.layout = html.H1(children= "My dash application.")  # skapar en layout för webbapplikationen
-my_H2 = html.H2(children = "More info ...")  # Behöver ej skriva ut children, men bara för tydlighetsskull
-my_dropdown = dcc.Dropdown(options = ["Äpple", "Päron", "Apelsin"], value="Päron")
-app.layout = html.Div(children = [my_H1, my_H2, my_dropdown]) # layout del: Hur komponenteran ligger i förhållande till varandra
-
-
-@callback(
-        Output(my_H2, component_property="children"),     # Property children vill vi ändra på
-        Input(my_dropdown, component_property="value")
-)
-def update_heading2(fruit):
-    return fruit.upper()
-
-
-# Tre delar man egentligen vill göra
-# hmtl komponenter, dash har egna också
-# layout
-# Funktionalitet som gör att ngt händer när man klickar någonstans: callbacks
+# Skapar komponenter
+my_H1 = html.H1(children= "My dash application.")  # Skapar en rubrik komponent, Behöver ej skriva ut children, men bara för tydlighetsskull
+my_H2 = html.H2(children = "More info ...")  # Skapar underrubrik komponent
+my_dropdown = dcc.Dropdown(options = ["Äpple", "Päron", "Apelsin"], value="Päron") # Skapar en dropdown komponent, options är en lista med alternativ, value är default värde
 
 # Detta genererar egentligen (kan antingen skrivas såhär i html fil eller via dash html komponenter):
 # <div>
 #   <h1>My dash application.</h1>
 #   <h2>More info ...</h2>
 # </div>
+# och sedan liknande HTML kod med olika css styling för att skapa buttom
 # Man skriver med dessa komponenter för att kunna köra med pythonkod.
 # https://dash.plotly.com/dash-html-components
-
 # Children: Berättar vilka taggar som finns inom H1. Flesta komponenter har den, berättar vad som ingår i komponenten.
 # H1 är en tagg som finns i html
-# Det man säger här är att man ska skriva h1 taggen med texten "My dash app.": <h1>My dash app.</h1>
-# Då den loopar löpande när den körs uppdaterar den det man ändrar här live.
+
+
+# Lägger till komponenter i layouten
+app.layout = html.Div(children = [my_H1, my_H2, my_dropdown]) # layout del: Hur komponenteran ligger i förhållande till varandra
+
+
+# Funktionalitet
+@callback(   # callback är en property som finns i Dash, den tar in en lista med inputs och outputs
+        Output(my_H2, component_property="children"),    # Output är en property som finns i Dash, den tar in en komponent och en property 
+        Input(my_dropdown, component_property="value")   # Input är en property som finns i Dash, den tar in en komponent och en property
+)                                                        
+def update_heading2(fruit):                              # Funktion som tar in en frukt och returnerar den med stora bokstäver
+    return fruit.upper()                                 
+
+
 
 app.run(debug=True) 
 # Den startar servern på med flask och renderar webbapplikationen
-# Flask är applikationen som startar servern och renderar det på en webbläsare
-# startar servern och renderar webbapplikationen, är igång nu med loop. 
-# Debug Lägger till knappen nere till höger i 
-# webbläsarsidan. Litet debug verktyg man har. Laddar även om automatiskt så fort man uppdaterar den.
+    # Då den loopar löpande när den körs uppdaterar den det man ändrar här live.    
+# Debug Lägger till knappen nere till höger i webbläsarsidan. Litet debug verktyg man har. Laddar även om automatiskt så fort man uppdaterar den.
 
 # För att stänga av servern tryck ctrl + c i terminalen
 
-# host=0.0.0.0, så kan man gå in i en lokal server på en annan dator. 
-# kommando ipconfig i powershell, terminal. IPv4 adress wirelsess tittar man på då : porten
-# Gör man det själv behöver man öppna porten.
-# Tillåter varning.
-# Man kan göra så och köra igång på dator, sedan ansluta med mobil eller liknande hemma.
+# Gå in i en lokal server på annan dator
+    # host=0.0.0.0, så kan man gå in i en lokal server på en annan dator. 
+    # kommando ipconfig i powershell, terminal. IPv4 adress wirelsess tittar man på då : porten
+    # Gör man det själv behöver man öppna porten.
+    # Tillåter varning.
+    # Man kan göra så och köra igång på dator, sedan ansluta med mobil eller liknande hemma.
 
+#Mer om detta:
+
+# Diverse anteckningar:
+    # Bli värd så andra kan gå in i ens server:
+        # host=0.0.0.0, jupyter external så kan man gå in i en lokal server på en annan dator. 
+        # kommando ipconfig i powershell, terminal. IPv4 adress wirelsess tittar man på då : porten
+        # Gör man det själv behöver man öppna porten.
+        # Tillåter varning.
+        # Man kan göra så och köra igång på dator, sedan ansluta med mobil eller liknande hemma.
+
+
+    #IP adress is built on:
+        # 1. The network adress
+        # 2. The subnet mask
+        # 3. The host adress
+        # 4. The port number
+
+    # I vårt fall där vi kör lokalt på datorn:
+        # IP Nummer pekar på adress till datorn, och porten pekar på adress till tjänsten på datorn
+        # Kör man redan något på 8050, så väljer man annan port.
+        # Kör på port 8050, komplierad diskussion tyckte han om hur man ska veta portnummer
 
 
 
